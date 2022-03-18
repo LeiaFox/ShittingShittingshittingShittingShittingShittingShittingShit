acf1b1f5919daa4a5db0e39938c0d4eaea6fe1c2
Leonardo Martinez Davydov <40177373@hcuc.ac.uk>
2022-03-18 11:27:09 +0000

    sgf

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import termcolor 
 
 df_capitals = pd.DataFrame([['England', 'London'], ['Austria','Vienna'],['France','Paris'],['Italy','Rome'],['Germany','Berlin'],['Switzerland','Bern'],['Belgium','Brussels']], columns=['Country', 'Capital '])
 
