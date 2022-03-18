b11da40b7fea5c93c697cd685072440ab7b4ddc1
LeiaFox <50370419+LeiaFox@users.noreply.github.com>
2022-03-18 11:57:34 +0000

    padn

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
-df_capitals = pd.DataFrame([['England', 'London'], ['Austria','Vienna'],['France','Paris'],['Italy','Rome'],['Germany','Berlin'],['Switzerland','Bern'],['Belgium','Brussels']], columns=['Country', 'Capital '])
+df_capitals = pd.DataFrame([['England', 'London'], ['Austria','Vienna'],['France','Paris'],['Italy','Rome'],['Germany','Berlin'],['Switzerland','Bern'],['Belgium','Brussels']], columns=['Country','Capital '])
 
-df_capitals.to_csv(r"C:\Sharing\Capitals.csv")
+df_capitals.to_csv(r"C:\Sharing\Capitals.csv", index = False)
+
+df = pd.read_csv(r"C:\Sharing\Capitals.csv")  
+
+print(df)
